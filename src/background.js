e47edb669f2c4bf8919697cab9027605f5ffc735
bo.zhang <bo.zhang@rea-group.com>
2020-02-18 20:20:35 +0800

    add shutcut to enable at app mode

--- src/background.js
+++ src/background.js
@@ -9,3 +9,11 @@ chrome.browserAction.onClicked.addListener(() => {
 chrome.runtime.onConnect.addListener((port) => {
   wsBridge.openConnection(port);
 });
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'toggle') {
+    chrome.tabs.executeScript(null, {
+      file: 'scripts/content-script.js'
+    });
+  }
+});
